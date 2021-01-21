007dac44ef38cbdca6863e2a25e30947fe6c2140
seungwon <seungwon@seungwonui-MacBookAir.local>
2021-01-21 17:31:11 +0900

    # Update: client.js

--- client.js
+++ client.js
@@ -1,9 +1,11 @@
 const chatlog = document.getElementById("chatLogs");
 const formBtn = document.getElementById("formBtn");
 const chatForm = document.getElementById("chatForm");
+const clearBtn = document.getElementById("clearBtn");
 
 function buttonClicked() {
-  const socket = io("localhost:5500");
+  console.log(`${document.domain}:${location.port}`);
+  const socket = io(`${document.domain}:${location.port}`);
   socket.emit("login", {
     name: makeRandomName(),
     userid: "test_id",
@@ -11,31 +13,43 @@ function buttonClicked() {
 
   socket.on("login", (data) => {
     const msg = "<div><strong>" + data + "</strong> has joined</div>";
-    chatlog.innerHTML(msg);
+    chatlog.innerHTML = chatlog.innerHTML + msg;
   });
 
   socket.on("chat", (data) => {
     const msg =
-      "<div>" +
-      data.msg +
-      " : from <strong>" +
+      '<div id="chatMsg">' +
+      "<strong>" +
       data.from.name +
-      "</strong></div>";
-    chatlog.innerHTML(msg);
+      "</strong>" +
+      ": " +
+      "<div style='display:inline'>" +
+      data.msg +
+      "</div>";
+    chatlog.innerHTML = chatlog.innerHTML + msg;
+  });
+
+  socket.on("loggedOut", (name) => {
+    const msg = "<div><strong>" + name + "</strong> went out</div>";
+    chatlog.innerHTML = chatlog.innerHTML + msg;
   });
 
   formBtn.addEventListener("click", (event) => {
     event.preventDefault();
     const input = document.getElementById("msgForm");
-    chatForm.submit();
     socket.emit("chat", { msg: input.value });
     input.value = "";
+  });
+
+  clearBtn.addEventListener("click", (event) => {
+    event.preventDefault();
+    chatlog.innerHTML = "";
   });
 
   function makeRandomName() {
     let name = "";
     const possible = "abcdefghijklmnopqrstuvwxyz";
-    for (let i = 0; i < 3; i++) {
+    for (let i = 0; i < 7; i++) {
       name += possible.charAt(Math.floor(Math.random() * possible.length));
     }
     return name;
